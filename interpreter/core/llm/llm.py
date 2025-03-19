82654203c5bb1030feb378cc932831d36af49b7d
endolith <endolith@gmail.com>
2025-03-19 12:56:56 -0400

    LLM: gpt-4o-mini as default model, don't waste money

--- interpreter/core/llm/llm.py
+++ interpreter/core/llm/llm.py
@@ -54,8 +54,8 @@ class Llm:
         self.completions = fixed_litellm_completions
 
         # Settings
-        self.model = "gpt-4o"
-        self.temperature = 0.0
+        self.model = "gpt-4o-mini"
+        self.temperature = 0
 
         self.supports_vision = None  # Will try to auto-detect
         self.vision_renderer = (
