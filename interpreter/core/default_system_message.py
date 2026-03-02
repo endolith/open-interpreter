d1f5875ba4088a9f159be90d1844bb3d83182da0
endolith <endolith@gmail.com>
2026-03-02 17:36:53 -0500

    SYSMSG: Add guidance to avoid displaying secrets in API-only terminal sessions

--- interpreter/core/default_system_message.py
+++ interpreter/core/default_system_message.py
@@ -93,6 +93,8 @@ For long-running scripts, print status updates in the loop.
 
 If you run commands that dump large amounts of text to the console, output will be truncated to conserve tokens. Instead, check file sizes, print only the first few lines of a large file, grep or filter the outputs of commands to display only the part you're looking for, etc.
 
+API-only: Don't run code that will display secrets in the terminal.  The system will attempt to redact them in case you do it by accident.
+
 **When to return control to the user:**
 - When an explicit part of the request is done (file loaded, data obtained, analysis complete)
 - When you need a user decision between approaches
