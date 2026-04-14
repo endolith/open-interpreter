91b5ff5cf58bfaa4a8e694ab878f0de41a65555a
endolith <endolith@gmail.com>
2026-04-13 22:03:54 -0400

    Add blank lines before scan and run prompts for improved UI spacing

--- interpreter/terminal_interface/terminal_interface.py
+++ interpreter/terminal_interface/terminal_interface.py
@@ -310,8 +310,9 @@ def terminal_interface(interpreter, message):
                             if interpreter.safe_mode == "auto":
                                 should_scan_code = True
                             elif interpreter.safe_mode == "ask":
+                                print("", flush=True)
                                 response = prompt_choice(
-                                    "\n  Would you like to scan this code? (y/n)\n\n  ",
+                                    "  Would you like to scan this code? (y/n)\n\n  ",
                                     ("y", "n"),
                                 )
                                 if response == "y":
@@ -320,10 +321,13 @@ def terminal_interface(interpreter, message):
                         if should_scan_code:
                             scan_code(code, language, interpreter)
 
+                        # Blank line before approval: leading \n inside input() is often lost after
+                        # Rich/console output on Windows (cursor sits on last panel row).
+                        print("", flush=True)
                         run_prompt = (
-                            "\nWould you like to run this code? (y/n/e = edit)\n\n"
+                            "Would you like to run this code? (y/n/e = edit)\n\n"
                             if interpreter.plain_text_display
-                            else "\n  Would you like to run this code? (y/n/e = edit)\n\n  "
+                            else "  Would you like to run this code? (y/n/e = edit)\n\n  "
                         )
                         response = prompt_choice(run_prompt, ("y", "n", "e"))
 
