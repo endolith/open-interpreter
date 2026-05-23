4a59ca89de56cc919f4d438bd7b09b01a46187dc
endolith <endolith@gmail.com>
2026-05-22 21:26:39 -0400

    Add binary_stdio flag to support byte streams for Perl REPL execution

--- interpreter/core/terminal/languages/perl.py
+++ interpreter/core/terminal/languages/perl.py
@@ -4,6 +4,7 @@ from .subprocess_language import SubprocessLanguage
 
 
 class Perl(SubprocessLanguage):
+    binary_stdio = True
     file_extension = "pl"
     name = "perl"
     execute_tool_hint = (
@@ -18,13 +19,6 @@ class Perl(SubprocessLanguage):
 
     def preprocess_code(self, code):
         return code.rstrip() + "\n__OI_END__\n"
-
-    def write_block_to_stdin(self, code):
-        # text=True on Windows converts \n to \r\n; perl_repl.pl waits for __OI_END__
-        # and would hang forever if the marker line is __OI_END__\r\n.
-        payload = code if code.endswith("\n") else code + "\n"
-        self.process.stdin.buffer.write(payload.encode("utf-8"))
-        self.process.stdin.buffer.flush()
 
     def detect_active_line(self, line):
         if "##active_line" in line:
