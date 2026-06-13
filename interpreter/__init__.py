8c240fc8eaf47d965920cd043ca73ed325ba30f7
endolith <endolith@gmail.com>
2026-06-13 01:06:00 -0400

    Update OR_SITE_URL to use github.com/endolith/open-interpreter in docs and code

--- interpreter/__init__.py
+++ interpreter/__init__.py
@@ -3,7 +3,7 @@ import sys
 import warnings
 
 # OpenRouter (via LiteLLM): optional HTTP-Referer and app title for openrouter.ai rankings.
-os.environ.setdefault("OR_SITE_URL", "https://github.com/openinterpreter/open-interpreter")
+os.environ.setdefault("OR_SITE_URL", "https://github.com/endolith/open-interpreter")
 os.environ.setdefault("OR_APP_NAME", "Open Interpreter")
 
 # Suppress pydantic warning from litellm about fields being removed in V2
